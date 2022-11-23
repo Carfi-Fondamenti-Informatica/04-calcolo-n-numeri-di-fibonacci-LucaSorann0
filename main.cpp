d72f2b52c54eb96f03f9d196aa5e0c7d729b7855
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-11-23 18:00:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci (int n) {
+   if (n<2) {
+      cout << "errore" << endl;
+   } else {
+      int a = 0, b = 1, c = 0;
+      cout << a + b << endl;
+      for (int i=1; i<n; i++){
+         c = a + b;
+         cout << c << endl;
+         a = b;
+         b = c;
+      }}
+   return n;
+}
+
 int main() {
+   int n = 0;
+      cin >> n;
+   fibonacci (n);
    return 0;
 }
